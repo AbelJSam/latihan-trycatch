a7c837f74a5ea98a84b323238af4f7d849199504
AbelJSam <abeljs007@gmail.com>
2026-09-21 19:25:22 +0700

    UP

--- bin/latihan_trycatch.dart
+++ bin/latihan_trycatch.dart
@@ -1,8 +1,28 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
-void main(){
+void main() async{
+//   //fungsi future tanpa pengembalian nilai
+//   Future<void> tarikData() async {
+//     var respone = await http.get(
+//       Uri.parse('https://jsonplaceholder.typicode.com/comments'),
+//       );
+//       var data = jsonDecode(respone.body);
+//       print(data);
+// }
 
-  Future<void> tarikData() async {
-    
+  // await tarikData();
+  
+
+   // POIN: Membuat Future dengan pengembalian nilai (Return Value)
+  Future<List<dynamic>> tarikData() async {
+    var respone = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/comments'),
+    );    
+    var data = jsonDecode(respone.body);
+    return data; 
   }
+  List<dynamic> hasilData = await tarikData();
+  print(hasilData);
 }
